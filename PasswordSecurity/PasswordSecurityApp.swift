e3bcdf3fba5730866704e78c3788e2c1e573d6c0
Carl J Nash <carljnash@msn.com>
2023-05-09 18:03:46 +0100

    Initial Commit

---
+++ PasswordSecurity/PasswordSecurityApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordSecurityApp.swift
+//  PasswordSecurity
+//
+//  Created by Carl Nash on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PasswordSecurityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
